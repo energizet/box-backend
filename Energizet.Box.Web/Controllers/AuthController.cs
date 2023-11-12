4521d50087de7f587f8cc7f6b83b02a90b531a5e
energizet <a2437@yandex.ru>
2023-11-12 09:05:56 +0300

    vk auth

--- Energizet.Box.Web/Controllers/AuthController.cs
+++ Energizet.Box.Web/Controllers/AuthController.cs
@@ -1,6 +1,10 @@
+using System.Security.Claims;
 using Energizet.Box.Core;
+using Energizet.Box.Entities;
 using Energizet.Box.Exceptions;
+using Energizet.Box.Vk.Abstractions;
 using Energizet.Box.Web.Models.Auth;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Energizet.Box.Web.Controllers;
@@ -10,10 +14,28 @@ namespace Energizet.Box.Web.Controllers;
 public sealed class AuthController : ControllerBase
 {
 	private readonly AuthCases _authCases;
+	private readonly IVkProvider _vkProvider;
 
-	public AuthController(AuthCases authCases)
+	public AuthController(AuthCases authCases, IVkProvider vkProvider)
 	{
 		_authCases = authCases;
+		_vkProvider = vkProvider;
+	}
+
+	[HttpGet("[action]")]
+	[Authorize(Roles = "user")]
+	public async Task<ActionResult<VkUser>> Info(CancellationToken token)
+	{
+		try
+		{
+			var vkUserId = HttpContext.User.FindFirst(ClaimTypes.Sid)!.Value;
+			var vkUser = await _vkProvider.GetVkUser(vkUserId, token);
+			return vkUser;
+		}
+		catch (NotFoundException ex)
+		{
+			return NotFound(ex.ToString());
+		}
 	}
 
 	[HttpPost]

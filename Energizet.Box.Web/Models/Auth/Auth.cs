db259c352ebc407dd1bc262c7c3cbb5132635c6b
energizet <a2437@yandex.ru>
2023-11-15 10:37:53 +0300

    auth

--- Energizet.Box.Web/Models/Auth/Auth.cs
+++ Energizet.Box.Web/Models/Auth/Auth.cs
@@ -1,13 +1,18 @@
-﻿namespace Energizet.Box.Web.Models.Auth;
+﻿using System.Text.Json.Serialization;
+
+namespace Energizet.Box.Web.Models.Auth;
 
 public class AuthRequest
 {
 	public static readonly AuthRequest Empty = new();
 
 	public int Uid { get; set; }
+	[JsonPropertyName("first_name")]
 	public string FirstName { get; set; } = string.Empty;
+	[JsonPropertyName("last_name")]
 	public string LastName { get; set; } = string.Empty;
 	public string Photo { get; set; } = string.Empty;
+	[JsonPropertyName("photo_rec")]
 	public string PhotoRec { get; set; } = string.Empty;
 	public string Hash { get; set; } = string.Empty;
 }
